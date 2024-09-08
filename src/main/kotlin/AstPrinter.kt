fe1a3acc17bdffc410a6181a18b8cdd48f8ea5bd
Sererim <danielleymanw@gmail.com>
2024-09-08 18:31:23 +0300

    Last commit for today. Everything works correctly :)

--- src/main/kotlin/AstPrinter.kt
+++ src/main/kotlin/AstPrinter.kt
@@ -10,18 +10,37 @@ object AstPrinter : Expr.Visitor<String> {
 	}
 
 	override fun visitBinaryExpr(expr: Expr.Binary): String {
-		TODO("Not yet implemented")
+		return parenthesize(
+			expr.operator.lexeme,
+			expr.left,
+			expr.right
+		)
 	}
 
 	override fun visitGroupingExpr(expr: Expr.Grouping): String {
-		TODO("Not yet implemented")
+		return parenthesize("'group'", expr.expression)
 	}
 
 	override fun visitLiteralExpr(expr: Expr.Literal): String {
-		TODO("Not yet implemented")
+		return if (expr.value.equals(null)) "nil"
+					 else expr.value.toString()
 	}
 
 	override fun visitUnaryExpr(expr: Expr.Unary): String {
-		TODO("Not yet implemented")
+		return parenthesize(expr.operator.lexeme, expr.right)
+	}
+
+	private fun parenthesize(name : String, vararg exprs : Expr) : String {
+		val builder : StringBuilder = StringBuilder()
+
+		builder.append("( ").append(name)
+
+		exprs.forEach { expr ->
+			run {
+				builder.append(" ").append(expr.accept(this))
+			}
+		}
+		builder.append(" )")
+		return builder.toString()
 	}
 }
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,5 +2,13 @@ package org.example
 
 
 fun main(args : Array<String>) {
-	Lox.entry(args)
+	val expression : Expr = Expr.Binary(
+		Expr.Unary(
+			Token(TokenType.MINUS, "-", "", 1),
+			Expr.Literal(123)
+		),
+		Token(TokenType.STAR, "*", "", 1),
+		Expr.Grouping(Expr.Literal(45.67))
+	)
+	println(AstPrinter.print(expression))
 }
